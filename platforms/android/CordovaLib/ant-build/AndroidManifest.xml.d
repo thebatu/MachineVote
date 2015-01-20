c9af16df5a15f07424e68b3e31d56d401bac018e
unknown <thomas790@gmail.com>
2015-01-20 16:37:44 +0100

    Ajout page sujets

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\wamp\www\MachineVote\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\wamp\www\MachineVote\platforms\android\CordovaLib\AndroidManifest.xml \
--- platforms/android/assets/www/js/main.js
+++ platforms/android/assets/www/js/main.js
@@ -1,9 +1,14 @@
 $(function(){
 	$("#nombreSujets").hide();
 
-	$(".app").on( "tap", function(event){
+	$(".app").on( "click", function(event){
 		$( event.target ).addClass( "tap" );
 		$(".app.tap").hide();
 		$("#nombreSujets").show();
+	});
+	
+	$(".choixSujet").on("click", function(event){
+		$(".choixSujet").removeClass("selected");
+		$(event.target).addClass("selected");
 	});
 });
