8ec1ae35f3d2bd758ebd693d44a86af32c97ec6f
Annie Hip-Ki <annie.hipki@etu.u-bordeaux.fr>
2015-01-20 09:20:08 +0100

    plugin

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,7 +1,17 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/com.mesmotronic.plugins.fullscreen/www/AndroidFullScreen.js",
+        "id": "com.mesmotronic.plugins.fullscreen.AndroidFullScreen",
+        "clobbers": [
+            "AndroidFullScreen"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "com.mesmotronic.plugins.fullscreen": "0.9.0"
+}
 // BOTTOM OF METADATA
 });
