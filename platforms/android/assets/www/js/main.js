c1f4cb0fcc133f1f9fa2f8602112c3e28a629b46
Annie Hip-Ki <annie.hipki@etu.u-bordeaux.fr>
2015-01-21 17:46:10 +0100

    build

--- platforms/android/assets/www/js/main.js
+++ platforms/android/assets/www/js/main.js
@@ -1,26 +1,60 @@
 $(function(){
+	var couleursVote = new Array("#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#00FFFF", "#FF00FF");
 	$("#nombreSujets").hide();
+	$("#couleursSujets").hide();
+	$("#demarre_vote").hide();
+	$("#vote").hide();
+	var creation = false;
+	var nbrVotant = 5;
+	var nbtVote = 0;
 
-	$(".app").on( "click", function(event){
-		$( event.target ).addClass( "tap" );
-		$(".app.tap").hide();
-		$("#nombreSujets").show();
+	$("#ecranLogo").on( "click", function(event){
+		$("#ecranLogo").hide();
+		gotoSection("nombreSujets");
 	});
+
 	/* Liste des boutons récurrents */
-	var validation = $(".valider"); 
+	var button = $(".content .navigation"); 
 
 	// Gestion du clic sur les boutons de choix de chemin //
-	validation.click( function() {
+	button.click( function() {
 		$(this).closest("div").hide();
 		gotoSection($(this).attr("go"));
 	});
 
 	function gotoSection(key) {
-		var nouvelle = $("#"+key);
-		nouvelle.show();
+		gererAction(getAction(key));
+		$("#"+key).show();
+		var nbrSujet = $('.selected').attr('value');
+		if(key == "couleursSujets" && creation == false){	
+			initNombreSujet(nbrSujet);
+		} else if(key == "vote") {
+			for(var i = 0; i < nbrSujet ; i++){
+				$('#bulletins').append("<button class='bulletin' style='background-color:"+couleursVote[i]+"'>"+$('.intitule_vote').attr(name)+"</button>");
+			}
+		}
 	}
 
-	
+	function getAction(key) {
+		return $("#" + key + " action").attr("name");
+	}
+
+	function gererAction(actionName) {
+		switch (actionName) {
+			case "vider_couleursSujets" :
+				$("#sujets").empty();
+				creation = false;
+				break;
+		}
+	}
+
+	function initNombreSujet(nbr){
+		for(var i=0; i<nbr; i++){
+			$('#sujets').append("<canvas class='couleurSujet' style='background-color:"+couleursVote[i]+"'></canvas><input class='intitule_vote' id='sujet"+i+"' type='text' />");
+		}
+		creation = true;
+	}
+
 	$(".choixSujet").on("click", function(event){
 		$(".choixSujet").removeClass("selected");
 		$(event.target).addClass("selected");
