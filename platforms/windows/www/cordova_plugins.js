ada300d7eb9ff41e725f8fcce0c88625a40dcf62
unknown <thomas790@gmail.com>
2015-01-28 14:34:31 +0100

    Test plugin NativeTransitions

--- platforms/windows/www/cordova_plugins.js
+++ platforms/windows/www/cordova_plugins.js
@@ -1,7 +1,17 @@
-﻿cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/uk.co.ilee.nativetransitions/www/nativetransitions.js",
+        "id": "uk.co.ilee.nativetransitions.NativeTransitions",
+        "clobbers": [
+            "nativetransitions"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "uk.co.ilee.nativetransitions": "0.1.4"
+}
 // BOTTOM OF METADATA
 });
